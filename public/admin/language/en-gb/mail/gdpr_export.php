07cf0558f904b582a193d7bdd8afef58990c3474
lucasjkr <lucas@lucaskrupinski.com>
2019-03-23 12:59:56 -0400

    Initial Commit of Opencart

---
+++ public/admin/language/en-gb/mail/gdpr_export.php
@@ -0,0 +1,28 @@
+<?php
+// Text
+$_['text_subject']    = '%s - General Data Personal Request';
+$_['text_hello']      = 'Hello <strong>%s</strong>,';
+$_['text_user']       = 'User';
+$_['text_gdpr']       = 'Export Personal Data';
+$_['text_account']    = 'Account';
+$_['text_customer']   = 'Personal Information';
+$_['text_firstname']  = 'First Name';
+$_['text_lastname']   = 'Last Name';
+$_['text_telephone']  = 'Telephone';
+$_['text_company']    = 'Company';
+$_['text_address_1']  = 'Address 1';
+$_['text_address_2']  = 'Address 2';
+$_['text_postcode']   = 'Post Code';
+$_['text_city']       = 'City';
+$_['text_country']    = 'Country';
+$_['text_zone']       = 'Region / State';
+$_['text_addresses']  = 'Addresses';
+$_['text_address']    = 'Address';
+$_['text_history']    = 'Login History';
+$_['text_ip']         = 'IP';
+$_['text_date_added'] = 'Date Added';
+
+$_['text_thanks']   = 'Thanks,';
+
+// Entry
+$_['entry_email']     = 'E-Mail';
